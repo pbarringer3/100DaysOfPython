a9cca39f0940335f7901bddf2c61c84fcd9a1913
Patrick Barringer <patrick.barringer@gmail.com>
2021-08-30 16:48:03 -0400

    Completed Day 32, sending inspirational quotes on Mondays.

--- Day32/main.py
+++ Day32/main.py
@@ -1,17 +1,27 @@
 import smtplib
+from datetime import datetime
+import random
 
-with open("credentials.txt", "r") as credentials:
-    my_email = credentials.readline().strip()
-    password = credentials.readline().strip()
-    receiving_email = credentials.readline().strip()
+now = datetime.now()
+day = now.weekday()
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
+if day == 0:
+    with open("quotes.txt", 'r') as quotes_file:
+        quotes = quotes_file.readlines()
+        random_quote = random.choice(quotes)
 
-    connection.sendmail(
-        from_addr=my_email, 
-        to_addrs=receiving_email, 
-        msg="Subject:Hello\n\nThis is the body of my email."
-    )
-    connection.close()
+    with open("credentials.txt", "r") as credentials:
+        my_email = credentials.readline().strip()
+        password = credentials.readline().strip()
+        receiving_email = credentials.readline().strip()
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+
+        connection.sendmail(
+            from_addr=my_email, 
+            to_addrs=receiving_email, 
+            msg="Subject:Be Inspired\n\n"+random_quote
+        )
+        connection.close()
